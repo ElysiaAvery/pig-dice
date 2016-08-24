6f877de8273600c4797312d84884a4a2dccc5a69
Ewa Manek and Elysia Nason <elysia@>
2016-08-24 09:29:43 -0700

    Add die roll

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,15 @@ function Player (){
   this.overallScore = 0;
 }
 
+function Dice(){
+  this.roll = 0;
+}
+
+Dice.prototype.randomRoll = function(){
+  this.roll = Math.floor(Math.random() * (6) + 1);
+  return this.roll;
+}
+
 //<!-- Front End  -->
 $(document).ready(function(){
   $("form#inputForm").submit(function(event){
