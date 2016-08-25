1072f576780ce52276f37a0c9229c8ca4d1a6d30
Ewa Manek and Elysia Nason <ewa@>
2016-08-25 15:09:26 -0700

    Add extra die image for two dice variant

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ function ComputerPlayer(){
 }
 
 ComputerPlayer.prototype.rollYN = function(){
-  if(this.turnScore + this.overallScore > 10){
+  if(this.turnScore + this.overallScore > 100){
     return false;
   }
   var decide = Math.round(Math.random());
@@ -49,7 +49,7 @@ function Game(players, dice, doubleDice){
 Game.prototype.endGame = function(){
   this.gamesCount++;
   this.players.forEach(function(player){
-    if(player.overallScore >= 10){
+    if(player.overallScore >= 100){
       player.gamesWon++;
     }
     player.turnScore = 0;
@@ -93,7 +93,7 @@ Game.prototype.getRoll = function(){
 Game.prototype.endTurn = function(){
   this.currentPlayer.overallScore += this.currentPlayer.turnScore;
   this.currentPlayer.turnScore = 0;
-  if (this.currentPlayer.overallScore >= 10){
+  if (this.currentPlayer.overallScore >= 100){
     this.win = this.currentPlayer;
   } else {
     var currentPlayerIndex = this.players.indexOf(this.currentPlayer);
@@ -124,6 +124,24 @@ $(document).ready(function(){
 
   $("#game-modal").submit(function(event){
     event.preventDefault();
+    var gameRules = ["<h4>Rules of Play:</h4>" +
+                    "<p>Each turn, a player repeatedly rolls a die until either a 1 is rolled or the player decides to 'hold':</p>" +
+                    "<ol>" +
+                    "<li>If the player rolls a 1, they score nothing and it becomes the next player's turn.</li>" +
+                    "<li>If the player rolls any other number, it is added to their turn total and the player's turn continues.</li>" +
+                    "<li>If a player chooses to 'hold', their turn total is added to their score, and it becomes the next player's turn.</li>" +
+                    "</ol>" +
+                    "<p>The first player to score 100 or more points wins.</p>",
+                    "<h4>Rules of Play:</h4>" +
+                    "<p>Each turn, a player repeatedly rolls the dice until either a 1 is rolled or the player decides to 'hold':</p>" +
+                    "<ol>" +
+                    "<li>If the player rolls a 1, they score nothing and it becomes the next player's turn.</li>" +
+                    "<li>If the player rolls a double 1, they score nothing, lose their total score, and it becomes the next player's turn.</li>" +
+                    "<li>If the player rolls any other doubles, they must roll again.</li>" +
+                    "<li>If the player rolls any other number, it is added to their turn total and the player's turn continues.</li>" +
+                    "<li>If a player chooses to 'hold', their turn total is added to their score, and it becomes the next player's turn.</li>" +
+                    "</ol>" +
+                    "<p>The first player to score 100 or more points wins.</p>"];
     $("#player1-name").val();
     $("#player2-name").val();
     var player1Modal = $("#player1-name").val();
@@ -137,6 +155,11 @@ $(document).ready(function(){
     if($("#doubleDice").is(":checked")){
       game.doubleDice = true;
       game.dice.push(new Dice());
+      $("#rules").html(gameRules[1]);
+      $("#roll-result").html("<div class='row'><div class='col-sm-6'><img id='roll-img' class='img-responsive' src='img/die1.png'></div><div class='col-sm-6'><img id='roll-img2' class='img-responsive' src='img/die2-1.png'></div></div>");
+    } else {
+      $("#rules").html(gameRules[0]);
+      $("#roll-result").html("<img id='roll-img' class='img-responsive' src='img/die1.png'>");
     }
     $('#startModal').modal('hide');
     switchPlayer();
@@ -161,6 +184,19 @@ $(document).ready(function(){
     $("#turn-score").text("Total this turn: " + game.currentPlayer.turnScore);
     if(game.currentPlayer.isComputer){
       rollDicePics();
+      $("#roll-game").prop('disabled', function( i, val ) {
+        return true;
+      });
+      $("#hold-game").prop('disabled', function( i, val ) {
+        return true;
+      });
+    } else {
+      $("#hold-game").prop('disabled', function( i, val ) {
+        return false;
+      });
+      $("#roll-game").prop('disabled', function( i, val ) {
+        return false;
+      });
     }
   }
 
@@ -172,34 +208,45 @@ $(document).ready(function(){
   function rollDicePics(){
     $("#youWon").empty();
     var diceArray = ["die1.png", "die2.png", "die3.png", "die4.png", "die5.png", "die6.png"];
+    var dice2Array = ["die2-1.png", "die2-2.png", "die2-3.png", "die2-4.png", "die2-5.png", "die2-6.png"];
     $("#roll-img").addClass("animated infinite wobble");
-    var diceImg;
+    if(game.doubleDice){
+      $("#roll-img2").addClass("animated infinite wobble");
+    }
+    var diceImg, dice2Img;
     var myInterval = setInterval(function(){
       var diceIndex = Math.floor(Math.random()*6);
+      var dice2Index = Math.floor(Math.random()*6);
       diceImg = "img/" + diceArray[diceIndex];
+      dice2Img = "img/" + dice2Array[dice2Index];
       $("#roll-img").attr("src", diceImg);
+      $("#roll-img2").attr("src", dice2Img);
     }, 250);
     var myTimeout = setTimeout(function(){
       clearInterval(myInterval);
-      $("#roll-img").removeClass("infinite wobble");
-      $("#roll-img").addClass("bounce");
+      $("#roll-img").removeClass("infinite wobble").addClass("bounce");
+      if(game.doubleDice){
+        $("#roll-img2").removeClass("infinite wobble").addClass("bounce");
+      }
       var turnEnd = game.getRoll();
       diceImg = "img/" + diceArray[game.dice[0].roll-1];
       $("#roll-img").attr("src", diceImg);
+      if(game.doubleDice){
+        dice2Img = "img/" + dice2Array[game.dice[1].roll-1];
+        $("#roll-img2").attr("src", dice2Img);
+      }
+      $("#dice1out").text(game.dice[0].roll);
+      $("#dice2out").text(game.dice[1].roll);
       $("#turn-score").text("Total this turn: " + game.currentPlayer.turnScore);
       if(turnEnd){
         $("#youWon").html("<h1>Whoops you rolled a one, loser</h1>");
+        updateScores();
         $("#roll-img").on('animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd', function(e) {
           switchPlayer();
           $("#roll-img").off('animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd');
         });
       }
       else {
-        if(game.dice[0].roll === game.dice[1].roll){
-          $("#hold-game").prop('disabled', function( i, val ) {
-            return !val;
-          });
-        }
         if(game.currentPlayer.isComputer){
           if(game.dice[0].roll === game.dice[1].roll){
             setTimeout(rollDicePics, 2000);
@@ -209,6 +256,10 @@ $(document).ready(function(){
           } else {
             setTimeout(holdDice, 2000);
           }
+        } else if(game.dice[0].roll === game.dice[1].roll){
+          $("#hold-game").prop('disabled', function( i, val ) {
+            return !val;
+          });
         }
       }
     }, 1000);
@@ -219,15 +270,19 @@ $(document).ready(function(){
     updateScores();
     if(game.win){
       $("#youWon").html("<h2>Congratulations " + game.win.name + ", you won! Good job.</h2>");
-      $("#roll-img").removeClass("bounce");
-      $("#roll-img").addClass("flip");
+      $("#roll-img").removeClass("bounce").addClass("flip");
+      if(game.doubleDice){
+        $("#roll-img2").removeClass("bounce").addClass("flip");
+      }
       $("#roll-img").on('animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd', function(e) {
         var endTimeout = setTimeout(function(){
           game.endGame();
-          $("#current-player").text("Current Player: " + game.currentPlayer.name);
-          $("#turn-score").text("Total this turn: " + game.currentPlayer.turnScore);
+          switchPlayer();
           $("#youWon").empty();
           $("#roll-img").removeClass("flip");
+          if(game.doubleDice){
+            $("#roll-img2").removeClass("flip");
+          }
           $("#game-number").text(game.gamesCount);
           $("#player1games").text(game.players[0].gamesWon);
           $("#player2games").text(game.players[1].gamesWon);
